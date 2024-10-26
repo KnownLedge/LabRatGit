014f1836bc59cec55907e099294c2365b846117a
KnownLedge <knowlegekeeper360@gmail.com>
2024-10-26 16:31:50 +0100

    Created options for Airborne movement
    Movement script now has enum for: Locked, Steer, Speed and Free movement referring to what the player can control while jumping

--- Assets/Ratmovement.cs
+++ Assets/Ratmovement.cs
@@ -14,6 +14,17 @@ public class Ratmovement : MonoBehaviour
     public float moveSpeed = 20f;
     public float jumpPower = 600f;
     public float jumpForce = 16f;
+
+    public enum jumpFreedom
+    {
+        Locked,
+        SteerAllowed,
+        SpeedControl,
+        FreeMovement
+    }
+
+    public jumpFreedom jumpStyle = jumpFreedom.Locked;
+
     void Start()
     {
       
@@ -36,27 +47,34 @@ public class Ratmovement : MonoBehaviour
         //  angle -= 90f;
         //The cannon defaults facing up, so we need to turn the angle to accomodate
 
-        if(moveState){
-
-        transform.rotation = Quaternion.Euler(new Vector3(0, -angle, 0));
-        //Sets the cannon pivot's rotation to the angle we made, turning the cannon through its parent
-
-        
-
-        if (Input.GetKey(KeyCode.Mouse0) || Input.GetKey(KeyCode.W))
+        if (moveState || jumpStyle != jumpFreedom.Locked) //steer, speed and free can pass 
         {
-          //  transform.Translate(transform.right * moveSpeed * Time.deltaTime, Space.World);
-            rb.AddForce(transform.right * moveSpeed);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(transform.forward * moveSpeed * Time.deltaTime, Space.World);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(transform.forward * -moveSpeed * Time.deltaTime, Space.World);
-        }
 
+            if (moveState || jumpStyle != jumpFreedom.SpeedControl) // steer and free can pass
+            {
+
+                transform.rotation = Quaternion.Euler(new Vector3(0, -angle, 0));
+                
+
+            }
+
+            if (moveState || jumpStyle != jumpFreedom.SteerAllowed) // speed and free can pass
+            {
+
+                if (Input.GetKey(KeyCode.Mouse0) || Input.GetKey(KeyCode.W))
+                {
+                    //  transform.Translate(transform.right * moveSpeed * Time.deltaTime, Space.World);
+                    rb.AddForce(transform.right * moveSpeed);
+                }
+                if (Input.GetKey(KeyCode.A))
+                {
+                    transform.Translate(transform.forward * moveSpeed * Time.deltaTime, Space.World);
+                }
+                if (Input.GetKey(KeyCode.D))
+                {
+                    transform.Translate(transform.forward * -moveSpeed * Time.deltaTime, Space.World);
+                }
+            }
         }
     
         if(Input.GetKeyDown(KeyCode.Space)){
@@ -92,5 +110,6 @@ public class Ratmovement : MonoBehaviour
 
 Unlock rat rotation for easier ramp access (also makes the rat hop when flipped, explore this)
 Lock rat rotation when jumping (make this an option, making the cube flip is funny)
-Create three different settings for jump controls (locked, steering allowed, free, etc)
+Create three different settings for jump controls (locked, steering allowed, free, etc) DONE
+Investigate different force movement to allow different jump settings to have more options (air steering is currently useless)
 */
