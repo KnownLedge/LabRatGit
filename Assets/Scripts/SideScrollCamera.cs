ffd0c0c544a6b8f146cd08d329bc8f32840f7446
Erk7 <157765285+Erk7@users.noreply.github.com>
2024-11-24 17:24:50 +0000

    Made new test scene for testing challenges + added forwards/backwards function to SideScrollCamera
    the rotation of the camera makes it kinda redundant but might as well keep it

--- Assets/Scripts/SideScrollCamera.cs
+++ Assets/Scripts/SideScrollCamera.cs
@@ -14,23 +14,31 @@ public class SideScrollCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.A))
+        if(Input.GetKey(KeyCode.A)) //move left
         {
             transform.Translate(-transform.right * camspeed * Time.deltaTime, Space.World);
         }
-        if(Input.GetKey(KeyCode.D))
+        if(Input.GetKey(KeyCode.D)) //move right
         {
             transform.Translate(transform.right * camspeed * Time.deltaTime, Space.World);
         }
 
-        if (Input.GetKey(KeyCode.Q))
+        if (Input.GetKey(KeyCode.Q)) //move up
         {
-            transform.Translate(-transform.up * camspeed * Time.deltaTime, Space.World);
+            transform.Translate(transform.up * camspeed * Time.deltaTime, Space.World);
         }
-        if (Input.GetKey(KeyCode.E))
+        if (Input.GetKey(KeyCode.E)) //move down
         {
-            transform.Translate(transform.up* camspeed * Time.deltaTime, Space.World);
+            transform.Translate(-transform.up* camspeed * Time.deltaTime, Space.World);
         }
 
+        if (Input.GetKey(KeyCode.Z)) //move forwards
+        {
+            transform.Translate(transform.forward * camspeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey(KeyCode.C)) //move backwards
+        {
+            transform.Translate(-transform.forward * camspeed * Time.deltaTime, Space.World);
+        }
     }
 }
