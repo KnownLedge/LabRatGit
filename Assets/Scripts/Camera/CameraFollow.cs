c1e84d302e0e842b41954dcbdd86d614168ab3f1
KnownLedge <knowlegekeeper360@gmail.com>
2025-02-10 16:48:02 +0000

    Cinematic camera (Readded once more)
    Cinemachine camera set up in lab 3 test 2, script called camera follow created to control it

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+//using Cinemachine.Utility;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject obj_path;
+    public GameObject obj_doll;
+    public Transform player;
+    public Transform realCamera;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var path = obj_path.GetComponent<CinemachineSmoothPath>();
+        var doll = obj_doll.GetComponent<CinemachineDollyCart>();
+        doll.m_Position = path.FindClosestPoint(player.position, 1, -1, 20);
+        realCamera.LookAt(player.position);
+    }
+}
