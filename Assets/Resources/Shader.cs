0a4d493c9f8a1af450136ce00a43c43b7ec2c84e
Tylerbrass <tylerbrass843@gmail.com>
2025-02-16 20:00:05 +0000

    ShaderUpdate
    A bug which caused the colour of the silhouette to change without any developer interaction

--- Assets/Resources/Shader.cs
+++ Assets/Resources/Shader.cs
@@ -1,25 +1,23 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using TreeEditor;
 using UnityEngine;
 
 public class Shader : MonoBehaviour
 {
     private HashSet<Mesh> rMeshes = new HashSet<Mesh>();
 
-    private List<Mesh> meshes = new List<Mesh>();
-    private List<listVector3> meshList = new List<listVector3>();
-    
+    private readonly List<Mesh> meshes = new List<Mesh>();
+    private readonly List<ListVector3> meshList = new List<ListVector3>();
 
     [Serializable]
-    private class listVector3
+    private class ListVector3
     {
         public List<Vector3> data;
     }
 
-    Material fillMaterial, maskMaterial;
-    Renderer[] renderers;
+    private Material fillMaterial, maskMaterial;
+    private Renderer[] renderers;
 
     public Color Fill
     {
@@ -30,92 +28,53 @@ public class Shader : MonoBehaviour
             UpdateShader = true;
         }
     }
+
     [SerializeField]
     private Color FillColor = Color.white;
 
     private bool UpdateShader;
+
     private void Awake()
     {
         renderers = GetComponentsInChildren<Renderer>();
-        fillMaterial = Instantiate(Resources.Load<Material>(@"Materials/fill"));
-        maskMaterial = Instantiate(Resources.Load<Material>(@"Materials/mask"));
+        fillMaterial = Instantiate(Resources.Load<Material>(@"Materials/FillMat"));
+        maskMaterial = Instantiate(Resources.Load<Material>(@"Materials/MaskMat"));
         fillMaterial.name = "Fill(instance)";
         maskMaterial.name = "Mask(instance)";
 
-        genSmoothNormals();
-
+        GenSmoothNormals();
 
         UpdateShader = true;
     }
 
-    //when the game object is set to be enabled 
-    private void OnEnable()
-    {
-        //loop throught the renders 
-        foreach (var render in renderers)
-        {
-            //create a list of gameobject current materials 
-            List<Material> materials = render.sharedMaterials.ToList();
-            //add the shaders
-
-            materials.Add(fillMaterial);
-            materials.Add(maskMaterial);
-            render.materials = materials.ToArray();
-        }
-    }
-   
     void OnValidate()
     {
-
-        // Update material properties
         UpdateShader = true;
-
-        // Clear cache when baking is disabled or corrupted
         if (meshes.Count != 0 || meshes.Count != meshList.Count)
         {
             meshes.Clear();
             meshes.Clear();
         }
-
-        // Generate smooth normals when baking is enabled
-        if (meshes.Count == 0)
-        {
-            bake();
-        }
     }
+
     private void Update()
     {
         //check if the shader needs updated
         if (UpdateShader)
         {
-            UpdateShader = false;
             UpdateMatPro();
+            UpdateShader = false;
         }
     }
-    void bake()
-    {
-        var bakedMeshes = new HashSet<Mesh>();
-        foreach (var mesh in GetComponentsInChildren<MeshFilter>())
-        {
-            if (!bakedMeshes.Add(mesh.sharedMesh))
-            {
-                continue;
-            }
-            var smoothNormals = SmoothNormals(mesh.sharedMesh);
 
-            rMeshes.Add(mesh.sharedMesh);
-            meshList.Add(new listVector3() { data  = smoothNormals });
-
-        }
-    }
-    void genSmoothNormals()
+    private void GenSmoothNormals()
     {
         foreach (var meshFiller in GetComponentsInChildren<MeshFilter>())
         {
             var currentMesh = meshFiller.sharedMesh;
             if (!rMeshes.Add(currentMesh))
             {
-                continue ;
+                continue;
             }
             // Retrieve or generate smooth normals
             var index = meshes.IndexOf(meshFiller.sharedMesh);
@@ -123,34 +82,21 @@ public class Shader : MonoBehaviour
 
             // Store smooth normals in UV3
             meshFiller.sharedMesh.SetUVs(3, smoothNormals);
-
-            // Combine submeshes
-            var renderer = meshFiller.GetComponent<Renderer>();
-                
-            if (renderer != null)
-            {
-                CombSubMesh(meshFiller.sharedMesh, renderer.sharedMaterials);
-            }
         }
 
         // Clear UV3 on skinned mesh renderers
         foreach (var skinnedMeshRenderer in GetComponentsInChildren<SkinnedMeshRenderer>())
         {
-
             // Skip if UV3 has already been reset
             if (!rMeshes.Add(skinnedMeshRenderer.sharedMesh))
             {
                 continue;
             }
-
             // Clear UV3
             skinnedMeshRenderer.sharedMesh.uv4 = new Vector2[skinnedMeshRenderer.sharedMesh.vertexCount];
-
-            // Combine submeshes
-            CombSubMesh(skinnedMeshRenderer.sharedMesh, skinnedMeshRenderer.sharedMaterials);
-
         }
     }
+
     List<Vector3> SmoothNormals(Mesh mesh)
     {
         // Group vertices by location
@@ -176,9 +122,7 @@ public class Shader : MonoBehaviour
             {
                 smoothNormal += smoothNormals[pair.Value];
             }
-
             smoothNormal.Normalize();
-
             // Assign smooth normal to each vertex
             foreach (var pair in group)
             {
@@ -189,29 +133,31 @@ public class Shader : MonoBehaviour
         return smoothNormals;
     }
 
-    void CombSubMesh(Mesh mesh, Material[] materials)
-    {
-        if(mesh.subMeshCount == 1)
-        {
-            return;
-        }
-        if(mesh.subMeshCount > materials.Length)
-        {
-            return;
-        }
 
-        mesh.subMeshCount++;
-        mesh.SetTriangles(mesh.triangles, mesh.subMeshCount - 1);
-    }
+    //update material properities
     void UpdateMatPro()
     {
         fillMaterial.SetColor("_OutlineColor", FillColor);
         maskMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.LessEqual);
         fillMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.Greater);
-
     }
 
-    private void OnDisable() 
+    //when the game object is set to be enabled 
+    private void OnEnable()
+    {
+        //loop throught the renders 
+        foreach (var render in renderers)
+        {
+            //create a list of gameobject current materials 
+            List<Material> materials = render.sharedMaterials.ToList();
+
+            //add the shaders
+            materials.Add(fillMaterial);
+            materials.Add(maskMaterial);
+            render.materials = materials.ToArray();
+        }
+    }
+    private void OnDisable()
     {
         Debug.Log("Disable");
         foreach (var render in renderers)
