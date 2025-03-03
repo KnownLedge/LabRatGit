89717749d729c14314427bad315f6d43f7349791
seancarter78 <130975354+seancarter78@users.noreply.github.com>
2025-03-03 20:31:39 +0000

    Updated collection system
    changes the way collectables work, now a popup appears instead of having to go through the inventory. Pressing I to enter the inventory still works for the sake of testing.

---
+++ Assets/Scripts/Rat/Collect System/PopupManager.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupManager : MonoBehaviour
+{
+    public static PopupManager instance;
+    private bool showPopup = false;
+    private string popupMessage = "";
+    private Sprite popupSprite = null;
+    private bool isPaused = false;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (showPopup && Input.GetKeyDown(KeyCode.F))
+        {
+            ClosePopup();
+        }
+    }
+
+    public void ShowPopup(string message, Sprite image)
+    {
+        popupMessage = message;
+        popupSprite = image;
+        showPopup = true;
+        isPaused = true;
+        Time.timeScale = 0; 
+    }
+
+    public void ClosePopup()
+    {
+        showPopup = false;
+        popupMessage = "";
+        popupSprite = null;
+        isPaused = false;
+        Time.timeScale = 1;
+    }
+
+    void OnGUI()
+    {
+        GUIStyle customStyle = new GUIStyle(GUI.skin.label);
+        customStyle.fontSize = 28;
+        customStyle.alignment = TextAnchor.MiddleCenter;
+        customStyle.normal.textColor = Color.white;
+        customStyle.hover.textColor = Color.white;
+
+        if (showPopup)
+        {
+            float screenWidth = Screen.width;
+            float screenHeight = Screen.height;
+
+            float imageWidth = screenWidth * 0.6f; 
+            float imageHeight = imageWidth * (9f / 16f); 
+            float imageX = (screenWidth - imageWidth) / 2;
+            float imageY = (screenHeight - imageHeight) / 2;
+
+            if (popupSprite != null)
+            {
+                GUI.DrawTexture(new Rect(imageX, imageY, imageWidth, imageHeight), popupSprite.texture);
+            }
+
+            float textWidth = imageWidth * 0.8f;
+            float textHeight = imageHeight * 0.4f;
+            float textX = imageX + (imageWidth - textWidth) / 2;
+            float textY = imageY + (imageHeight - textHeight) / 2;
+
+            GUI.Label(new Rect(textX, textY, textWidth, textHeight), popupMessage, customStyle);
+        }
+    }
+}
