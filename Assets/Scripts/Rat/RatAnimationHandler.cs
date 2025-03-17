bdd6ced54e0d9e395aeffd9868bb66e7871a755e
KnownLedge <knowlegekeeper360@gmail.com>
2025-03-17 00:29:58 +0000

    Rat running animation!
    Imported Hollys run animation, then connected it up to an animation controller and made a script to move rat based on forward velocity

---
+++ Assets/Scripts/Rat/RatAnimationHandler.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using UnityEditor;
+using UnityEditor.Animations;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatAnimationHandler : MonoBehaviour
+{
+    // Script for animating rat, might be better just doing this in ratmovement if this gets too complex
+
+    public Animator animControl;
+    public float speedMult = 1;
+    public Rigidbody rb;
+    private float forwardSpeed = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 LocalVelocity = (transform.InverseTransformDirection(rb.velocity));
+        animControl.SetFloat("ForwardSpeed",  LocalVelocity.z * speedMult);
+    }
+}
