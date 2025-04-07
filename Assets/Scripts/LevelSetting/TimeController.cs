42332804ae9e72bfd598e4fe806ac86d1c1f1aa8
ikryzhanovskyi <ilyuhamalyuha@gmail.com>
2025-04-07 11:18:55 +0100

    Time Manager
    Added script to change time in game

---
+++ Assets/Scripts/LevelSetting/TimeController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            SetTimeScale(1f, "Normal Speed");
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+            SetTimeScale(0.75f, "75% Speed");
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+            SetTimeScale(0.5f, "Half Speed");
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+            SetTimeScale(0.25f, "Quarter Speed");
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+            SetTimeScale(0.1f, "10% Speed");
+        else if (Input.GetKeyDown(KeyCode.Alpha0))
+            SetTimeScale(0f, "Paused");
+    }
+
+    void SetTimeScale(float newScale, string label)
+    {
+        Time.timeScale = newScale;
+        Time.fixedDeltaTime = 0.02f * Time.timeScale;
+        Debug.Log("Time Scale set to " + newScale + " (" + label + ")");
+    }
+}
