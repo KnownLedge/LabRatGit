22e2701a2937f04204e3ca802c2eae969a01647d
Illia Kryzhanovskyi <ilyuhamalyuha@gmail.com>
2025-04-08 12:52:24 +0100

    SFX for bird attack
    Added sound effects when bird is attacking player

--- Assets/Scripts/Enemies/Birds/BirdTrigger.cs
+++ Assets/Scripts/Enemies/Birds/BirdTrigger.cs
@@ -2,16 +2,34 @@ using UnityEngine;
 
 public class BirdTrigger : MonoBehaviour
 {
+    [SerializeField] private AudioClip birdSound;
     [SerializeField] private Bird bird; 
     [SerializeField] private GameObject player;
 
+    private AudioSource audioSource;
     private bool playerInside = false;
+
+    private void Start()
+    {
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        audioSource.playOnAwake = false;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == player)
         {
             playerInside = true;
+
+            if (birdSound != null)
+            {
+                audioSource.clip = birdSound;
+                audioSource.Play();
+            }
 
             if (!bird.IsAttackingOrReturning())
             {
@@ -25,6 +43,11 @@ public class BirdTrigger : MonoBehaviour
         if (other.gameObject == player)
         {
             playerInside = false;
+            if (birdSound != null)
+            {
+                audioSource.clip = birdSound;
+                audioSource.Stop();
+            }
         }
     }
 
