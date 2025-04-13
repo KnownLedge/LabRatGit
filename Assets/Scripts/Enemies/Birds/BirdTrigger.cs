38489a7777dabc66c385420a118b17741fd677f7
ikryzhanovskyi <ilyuhamalyuha@gmail.com>
2025-04-13 23:24:39 +0100

    Bird and bird trigger fix | Player Respawn fix
    Fixed player respawn causing rat flying after being hit by bird. Also fixed bug when player could be respawned even when bird is not attacking

--- Assets/Scripts/Enemies/Birds/BirdTrigger.cs
+++ Assets/Scripts/Enemies/Birds/BirdTrigger.cs
@@ -3,22 +3,32 @@ using UnityEngine;
 public class BirdTrigger : MonoBehaviour
 {
     [SerializeField] private AudioClip birdSound;
-    [SerializeField] private Bird bird; 
+    [SerializeField] private Bird bird;
     [SerializeField] private GameObject player;
     [SerializeField] private AudioManager audioManager;
+
     private bool playerInside = false;
+    private bool hasRespawned = false;
+    private Collider triggerCollider;
+
+    private void Start()
+    {
+        triggerCollider = GetComponent<Collider>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == player)
         {
             playerInside = true;
+            hasRespawned = false;
 
             if (birdSound != null)
             {
                 audioManager.PlaySFX(birdSound);
             }
 
-            if (!bird.IsAttackingOrReturning())
+            if (!bird.IsAttacking())
             {
                 bird.StartAttack();
             }
@@ -35,9 +45,15 @@ public class BirdTrigger : MonoBehaviour
 
     private void Update()
     {
-        if (bird.AttackFinished() && playerInside)
+        if (playerInside && !hasRespawned && bird.IsAttacking() && IsBirdInsideTrigger())
         {
-            bird.RespawnPlayer(player);
+            hasRespawned = true;
+            StartCoroutine(bird.RespawnPlayer(player));
         }
+    }
+
+    private bool IsBirdInsideTrigger()
+    {
+        return triggerCollider.bounds.Contains(bird.transform.position);
     }
 }
